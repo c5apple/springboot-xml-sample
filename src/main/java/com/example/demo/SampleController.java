d96656dd79d834fb321921df942021013cffd3db
c5apple <c5apple_party@yahoo.co.jp>
2020-02-17 12:55:07 +0900

    サンプルコントローラ

---
+++ src/main/java/com/example/demo/SampleController.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+
+	private static List<SampleResource> store = new ArrayList<SampleResource>();
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<SampleResource> post(@ModelAttribute SampleResource data,
+			UriComponentsBuilder uriComponentsBuilder) {
+		store.add(data);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(uriComponentsBuilder.path("/sample/{id}").buildAndExpand(store.size()).toUri());
+		return new ResponseEntity<SampleResource>(data, headers, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<SampleResource> get() {
+		return store;
+	}
+
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
+	public SampleResource get(@PathVariable int id) {
+		return store.get(id - 1);
+	}
+
+}
---
+++ src/main/java/com/example/demo/SampleResource.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+// Lombokを使えば良い
+@XmlRootElement
+public class SampleResource {
+
+	private Integer integer;
+	private String string;
+	private List<String> strings;
+
+	public Integer getInteger() {
+		return integer;
+	}
+
+	public void setInteger(Integer integer) {
+		this.integer = integer;
+	}
+
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+
+	public List<String> getStrings() {
+		return strings;
+	}
+
+	public void setStrings(List<String> strings) {
+		this.strings = strings;
+	}
+}
