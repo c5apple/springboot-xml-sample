ab2e96f9faf7d8a7594eda261066d02249bf823f
c5apple <c5apple_party@yahoo.co.jp>
2020-02-17 12:19:40 +0900

    initial commit

---
+++ src/main/java/com/example/demo/SpringbootXmlSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootXmlSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootXmlSampleApplication.class, args);
+	}
+
+}
